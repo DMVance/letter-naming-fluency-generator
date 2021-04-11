7e431ae3809e6ddf3788d27f35fa5503415faab8
DMVance <csljrrt@gmail.com>
2021-04-11 10:41:39 -0500

    get user input functionality

--- letters.py
+++ letters.py
@@ -19,20 +19,27 @@ import string, random
 # @app.route("/places")
 def letters_gen():
 
-    lowercase_alphabet = list(string.ascii_lowercase)
-    uppercase_alphabet = list(string.ascii_uppercase)
-    upper_lower_mix_alphabet = list(string.ascii_letters)
+    # print("Please enter the number of letters and press [Enter]: ")
+    # n = input("--> ")
+
+    print("Lowercase, Uppercase or Mixed [L, U, M]: ")
+    m = input("--> ")
+
+    if m == "L":
+        alphabet = list(string.ascii_lowercase)
+    elif m == "U":
+        alphabet = list(string.ascii_uppercase)
+    elif m == "M":
+        alphabet = list(string.ascii_letters)
 
     n = 26
-    m = 52
+    # m = 52
 
-    random_lowercase = random.sample(lowercase_alphabet, n)
-    random_uppercase = random.sample(uppercase_alphabet, n)
-    random_mix = random.sample(upper_lower_mix_alphabet, m)
+    random_letters = random.sample(alphabet, n)
 
-    test = random_lowercase[:5]
+    # test = random_letters[:5]
 
-    print(', '.join(random_lowercase[:5]))
+    # print(', '.join(random_lowercase[:5]))
     # print(', '.join(random_lowercase[5:10]))
     # print(', '.join(random_lowercase[10:15]))
     # print(', '.join(random_lowercase[15:20]))
@@ -55,12 +62,24 @@ def letters_gen():
     # print(', '.join(random_mix[40:45]))
     # print(', '.join(random_mix[45:52]))
 
-    return ' '.join(random_lowercase[:5])
+    return ' '.join(random_letters)
+
+# def main():
+
+#     # Get the word(s) from the user
+#     print("Please enter the words and press [Enter]: ")
+#     words = input("--> ").lower()
+    
+#     # split the string of words into individual words and eliiminate unnecessary characters
+#     words = words.replace("-", " ").replace(",", " ").replace("*", " ").split(" ") # delimiter = any character in wrdset that isn't in alphabet
+
 
 
 #######################################################################################
 
-# letters()
+letters_gen()
+
+# main()
 
 #######################################################################################
 

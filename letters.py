4df4a7a3125b1708c9e10aa6f0661bba539ae043
David Vance <vance06@yahoo.com>
2021-04-12 20:23:01 -0500

    solves validation for case input, not fully for lines input

--- letters.py
+++ letters.py
@@ -4,61 +4,31 @@ def generate_letters():
 
     p = 10
     
-    # while True:
-    #     try:
-    #         print("Please enter the number of lines and press [Enter]: ")
-    #         lines = int(input("--> "))
-    #     except ValueError:
-    #         print("Error: invalid entry.")  # Research proper use of exception handling.
-    #         continue
-
-    #     if lines < 0:
-    #         print("Your response must be a positive number.")
-    #         continue
-    #     else:
-    #         break
-
-
-
-
-    # while True:
-    # try:
-    #     age = int(input("Please enter your age: "))
-    # except ValueError:
-    #     print("Sorry, I didn't understand that.")
-    #     continue
-
-    # if age < 0:
-    #     print("Sorry, your response must not be negative.")
-    #     continue
-    # else:
-    #     #age was successfully parsed, and we're happy with its value.
-    #     #we're ready to exit the loop.
-    #     break
-
     while True:
-    data = input("Please enter a loud message (must be all caps): ")
-    if not data.isupper():
-        print("Sorry, your response was not loud enough.")
-        continue
-    else:
-        #we're happy with the value given.
-        #we're ready to exit the loop.
+        print("Please enter the number of lines and press [Enter]:")
+        lines = int(input("--> "))
+        if lines < 0:
+            print("Your response must be a positive number.")
+            continue
         break
+    print(lines)
 
     while True:
-        data = input("Pick an answer from A to D:")
-        if data.lower() not in ('a', 'b', 'c', 'd'):
-            print("Not an appropriate choice.")
-        else:
-            break
+        print("Would you like Uppercase, Lowercase or Mixed [U, L, M]?")
+        case = input("--> ")
+        case = case.lower()
+        if case not in ('u', 'l', 'm'):
+            print("Please enter either 'U', 'L', or 'M'")
+            continue
+        break
+    print(case)
 
-    # if case == "l":
-    #     alphabet = list(string.ascii_lowercase)     # Can this section be collapsed?
-    # elif case == "u":
-    #     alphabet = list(string.ascii_uppercase)
-    # elif case == "m":
-    #     alphabet = list(string.ascii_letters)
+    if case == "l":
+        alphabet = list(string.ascii_lowercase)     # Can this section be collapsed?
+    elif case == "u":
+        alphabet = list(string.ascii_uppercase)
+    elif case == "m":
+        alphabet = list(string.ascii_letters)
 
     for i in range(lines):
         random_letters = random.sample(alphabet, p)

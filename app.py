2b1377db07416440c370aeedfdad11c2d036aafc
David Vance <vance06@yahoo.com>
2021-04-12 22:14:20 -0500

    prep to build JS for interactive page

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     output = generate_letters()
-    # return render_template("letter_generator.html")
     return render_template("parent.html", output=output)
-
-# @app.route("/places")
-# def places():
-#     return render_template("places.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

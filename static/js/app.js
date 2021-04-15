3c72e0d4c3fd425dd59e153505016a7bf2c7c7bd
David Vance <vance06@yahoo.com>
2021-04-14 20:47:46 -0500

    adding interactive features

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,8 @@
 
 console.log("app.js is running!")
 
+function getLines() {
+    console.log("Running Lines in app.py")
+    var userSelection = d3.select("#case-submit-button").property("value")
+    console.log(`User text: ${userSelection}')
+}

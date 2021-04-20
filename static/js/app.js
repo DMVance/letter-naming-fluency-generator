1387e3793c497734841eb0b8673c4587c6df92ad
DMVance <csljrrt@gmail.com>
2021-04-19 20:41:58 -0500

    fixed error in closing function - had wrong apostrophe type, syntax error

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,8 @@
 console.log("app.js is running!")
 
 function getLines() {
-    console.log("Running Lines in app.py")
-    var userSelection = d3.select("#case-submit-button").property("value")
-    console.log(`User text: ${userSelection}')
+    console.log("Running Lines in app.py");
+    var userSelection = d3.select("#case-submit-button").property("value");
+    console.log('User text: ${userSelection}');
+
 }

6073c6d827d0d3bd5bdca1e595f3413609dea89d
DMVance <csljrrt@gmail.com>
2021-04-21 21:55:33 -0500

    add GET and POST route

--- app.py
+++ app.py
@@ -9,7 +9,19 @@ def index():
     # output = generate_letters()
     return render_template("parent.html") # , output=output)
 
-# Placeholder for additional routes
+# Try this to transfer data to and from python and JS
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+
+    # GET request
+    if request.method == 'GET':  
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
 
 if __name__ == "__main__":
     app.run(debug=True)

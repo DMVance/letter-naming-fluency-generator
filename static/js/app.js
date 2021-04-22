4db1a982a56ac6b5c152c8efd492e1448c2a05e0
DMVance <csljrrt@gmail.com>
2021-04-21 19:45:43 -0500

    use text entry box for now for case until structure for generating letters is integrated

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,28 @@
 
 console.log("app.js is running!")
 
-function getLines() {
-    console.log("Running Lines in app.py");
-    var userSelection = d3.select("#case-submit-button").property("value");
-    console.log('User text: ${userSelection}');
-}
+// function getLines() {
+//     console.log("Running Lines in app.py");
+//     var userSelection = d3.select("#case-submit-button").property("value");
+//     console.log('User text: ${userSelection}');
+// }
 
-d3.select("#case-submit-button").on("click", getLines)
+d3.select("#lines-submit-button").on("change", (event) => {
+    var userLines = d3.event.target.value;
+    console.log("lines-submit received");
+    console.log(`Number of lines: ${userLines}`)
+    d3.select("body").append("p").text(userLines)
+})
+
+d3.select("#case-submit-button").on("change", (event) => {
+    var userCase = d3.event.target.value;
+    console.log("case-submit received");
+    console.log(`Case: ${userCase}`)
+    d3.select("body").append("p").text(userCase)
+})
+// d3.select("#case-submit-button").on("click", (event) => {
+//     var userCase = d3.event.target.value;
+//     console.log("case-submit received");
+//     console.log(`Case selected: ${userCase}`)
+//     d3.select("body").append("p").text(userCase)
+// })

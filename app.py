7f467527cf937420089cc390d784d500f7440b24
DMVance <csljrrt@gmail.com>
2021-04-25 08:34:29 -0500

    edit json messages for clarity

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def testfn():
 
     # GET request
     if request.method == 'GET':  
-        message = {'greeting':'Hello from Flask!'}
+        message = {'greeting':'Hello from app.py Flask!'}
         return jsonify(message)  # serialize and use JSON headers
 
     # POST request
     if request.method == 'POST':
         print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
+        return 'Sucesss at app.py', 200
 
 if __name__ == "__main__":
     app.run(debug=True)

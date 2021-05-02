e113e72e7cbe8a637a1a989e06ba3027010b5f9a
DMVance <csljrrt@gmail.com>
2021-05-02 11:22:11 -0500

    fixed fetch POST issue

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,44 @@
 console.log("app.js is running!")
 
 function getData() {
-    console.log("Request has been made to get user input to send to letters.py");
 
-    var userLines = d3.select("#lines-submit-button").property("value");
-    // console.log("lines-submit received");
+    let userLines = d3.select("#lines-submit-button").property("value");
     console.log(`Number of lines: ${userLines}`)
 
-    var userCase = d3.select("#case-submit-button").property("value")
+    let userCase = d3.select("#case-submit-button").property("value")
     console.log(`Case: ${userCase}`)
     d3.select("body").append("p").text(userCase)
 
-    let data = [userLines, userCase]
+    var data = [userLines, userCase]
     console.log(`The data array is: ${data}`)
-    
+
+    jsonArrData = JSON.stringify(data)
+    console.log(jsonArrData)
+    console.log(typeof jsonArrData === 'string')
+        
     // let table = d3.select("#letters-table").append("p").text(userLines)
     // table.selectAll("tr").remove() // remove all existing rows to reset, preventing posting data multiple times
     // let table = d3.select("#letters-table").append("p").text(userCase)
     // table.selectAll("tr").remove()
 
     fetch(`${window.origin}/test`, {
+    // fetch("http://127.0.0.1:5000", {
         method: "POST",
+        // mode: "cors",
         credentials: "include",
-        body: JSON.stringify(data),
-        cache: "no-cache",
         headers: new Headers({
             "content-type": "application/json"
-            })
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(function (error) {
-            console.log("Fetch error: " + error)
-        })
+        }),
+        body: JSON.stringify(data),
+        cache: "no-cache",
+    })
+    // .then(response => response.json()) # This was part of the problem!! response is not json serializable!
+    .then(data => {
+        console.log("Success, the data: ", data);
+    })
+    .catch((error) => {
+        console.error("Aggravating Error: ", error); // data is not making it to "/test"
+    });
 }
 
 // Once logic is working and data is flowing properly between scripts and 

31a7c47c696fc34b37f46463a52d67da419c96cc
DMVance <csljrrt@gmail.com>
2021-05-01 23:01:30 -0500

    successfully get case and lines from user input on page

--- letters.py
+++ letters.py
@@ -34,6 +34,11 @@ def generate_letters(lines, case):
 
     # lines = get_lines()  # Get from HTML input or app.js rather than function
     # case = get_case()    # Get from HTML input or app.js rather than function
+    case = case.lower()
+    lines = int(lines)
+
+    print("From letters.py, lines = ", lines)
+    print("From letters.py, case = ", case)
 
     if case == "l":
         alphabet = list(string.ascii_lowercase)     # Can this section be collapsed?
@@ -47,8 +52,10 @@ def generate_letters(lines, case):
     for i in range(lines):
         random_letters = random.sample(alphabet, p)
         output_list.append(random_letters)
-        print(output_list)
-        print(' '.join(random_letters))
+        # print(output_list)
+        # print(' '.join(random_letters))
+    
+    print(output_list)
 
     return jsonify(random_letters) # Return a JSON that can be pulled into JS.
 

91cb3f2bf55bbb9727dfe0d714a279fd569dc733
DMVance <csljrrt@gmail.com>
2021-05-06 20:56:20 -0500

    set up data for json to JS for plotly

--- letters.py
+++ letters.py
@@ -28,7 +28,32 @@ def generate_letters(lines, case):
 
     ##############################################
     # Set up the data for a Plotly table or other plot here. Send this back as JSON, rather than the raw data.
+    # var values = [
+    #   ['Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'],
+    #   [1200000, 20000, 80000, 2000, 12120000],
+    #   [1300000, 20000, 70000, 2000, 130902000],
+    #   [1300000, 20000, 120000, 2000, 131222000],
+    #   [1400000, 20000, 90000, 2000, 14102000]]
+
+    output_data = [{
+        "type": 'table',
+        "header": {
+            "values": ["<b>Lines</b>"],   #[ , ["<b>Q1</b>"], ["<b>Q2</b>"], ["<b>Q3</b>"], ["<b>Q4</b>"]],
+            "align": "center",
+            "line": {"width": 1, "color": "black"},
+            "fill": {"color": "grey"},
+            "font": {"family": "Arial", "size": 12, "color": "white"}
+        },
+        "cells": {
+            "values": output_list,
+            "align": "center",
+            "line": {"color": "black", "width": 1},
+            "font": {"family": "Arial", "size": 11, "color": ["black"]}
+        }
+    }]
+
+    # Plotly.newPlot('myDiv', data);
     ##############################################
 
-    # return jsonify(random_letters) # Return a JSON that can be pulled into JS.
-    return output_list
+    # return jsonify(random_letters) # Return a JSON that can be pulled into JS. --> Update: used jsonify() in app.py
+    return output_data     #output_list

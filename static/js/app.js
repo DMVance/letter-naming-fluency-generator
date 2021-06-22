d5e992c500d4c417b5c48d21aa83569633f2c5c5
DMVance <csljrrt@gmail.com>
2021-06-21 19:14:03 -0500

    add comments

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,11 @@ console.log("app.js is running!")
 
 function getData() {
 
+    // User inputs desired number of lines of letters
     let userLines = d3.select("#lines-submit-button").property("value");
     console.log(`app.js, Number of lines: ${userLines}`)
 
+    // User inputs desired case
     let userCase = d3.select("#case-submit-button").property("value")
     console.log(`app.js, Case: ${userCase}`)
     // d3.select("body").append("p").text(userCase)
@@ -12,6 +14,7 @@ function getData() {
     var data = [userLines, userCase]
     console.log(`app.js, The data array is: ${data}`)
 
+    // To pass the user input to the Python script where the output will be generated, data array is converted to JSON format for transfer.
     jsonArrData = JSON.stringify(data)
     // console.log('app.js, ', jsonArrData)
     console.log('app.js, jsonArrData is a string: ', typeof jsonArrData === 'string')
@@ -21,6 +24,7 @@ function getData() {
     // let table = d3.select("#letters-table").append("p").text(userCase)
     // table.selectAll("tr").remove()
 
+    // Here the JSON is posted to an endpoint where Python script pulls from. It is the intermediary between the two scripts.
     fetch(`${window.origin}/test`, {
         method: "POST",
         // mode: "cors",
@@ -33,12 +37,12 @@ function getData() {
     })
     // .then(response => response.json()) # This was part of the problem!! response is not json serializable!
     .then(data => {
-        console.log("app.js, Success! The data: ", data);
+        console.log("app.js, Success! The data: ", data); // When the output is returned from Python, the result is logged to the console
         data.json().then(responseJson => {
             console.log(responseJson)
-            d3.select(".output").selectAll("p").remove()
+            d3.select(".output").selectAll("p").remove() // Previous output is cleared.
             responseJson.forEach(e => {
-                d3.select(".output").append("p").text(Object.values(e).join(" ")).append("hr");
+                d3.select(".output").append("p").text(Object.values(e).join(" ")).append("hr"); // New output is displayed on page with horizontal line between each line of output.
             });
             // Plotly.newPlot("output", responseJson)  // only plotting the object, not parsed data
             })
